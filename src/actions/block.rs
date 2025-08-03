4718074c8513a2c732b9d92b5f5b1cbfe7940ae8
Arthur Eubanks <ayueubanks@gmail.com>
2025-08-03 14:35:24 -0700

    Cap block at 999

--- src/actions/block.rs
+++ src/actions/block.rs
@@ -55,12 +55,33 @@ impl Action for BlockAction {
         if self.modify {
             amount = calculate_modified_block(amount, &game.player.creature);
         }
-        game.get_creature_mut(self.target).block += amount
+        let c = game.get_creature_mut(self.target);
+        c.block += amount;
+        if c.block > 999 {
+            c.block = 999;
+        }
     }
 }
 
 impl std::fmt::Debug for BlockAction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "block {} {:?}", self.amount, self.target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        actions::block::BlockAction,
+        game::GameBuilder,
+    };
+
+    #[test]
+    fn test_999() {
+        let mut g = GameBuilder::default().build_combat();
+        g.run_action(BlockAction::player_flat_amount(1000));
+        assert_eq!(g.player.creature.block, 999);
+        g.run_action(BlockAction::player_card(1000));
+        assert_eq!(g.player.creature.block, 999);
     }
 }
