b929a97f992b1ef8bfd28d8e44211e00daa9712e
Arthur Eubanks <ayueubanks@gmail.com>
2025-09-17 13:07:08 -0700

    Runic dome TODO -> unsupported

--- src/relic.rs
+++ src/relic.rs
@@ -175,7 +175,7 @@ r!(
     FusionHammer => Boss, // TODO
     PandorasBox => Boss, // TODO
     PhilosophersStone => Boss, // TODO
-    RunicDome => Boss, // TODO
+    RunicDome => Boss, // not supported
     RunicPyramid => Boss, // TODO
     SacredBark => Boss,
     SlaversCollar => Boss, // TODO
